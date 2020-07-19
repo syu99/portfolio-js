7039ed4a7e3f0651e1f476cf27c1e635f6bf20c6
syu99 <ufcmjohan@gmail.com>
2020-07-19 14:34:46 +0200

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+class Portfolio{
+
+    /** * @param {string}selecteur */ 
+
+constructor(selecteur){
+     // Lorsque l'on clique sur un projet
+        // On clone le project__body
+    }
+}
+
+new Portfolio("#js-portfolio")
